5584b72a4b69fc7014b4b672c66c663e98afdc5e
Darcy Tyler <darcy.tyler@hotmail.com>
2025-08-22 23:13:25 +0800

    you can now successfully run flask and access the login page

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
-
-# You can configure app settings here, e.g. secret key
-app.config['SECRET_KEY'] = 'your_secret_key_here'
 
 from app import routes
